cdf9ab715daf2ddeac59a239d5aeefd3ae3b0a56
seymaergun <seymaergun89@gmail.com>
2023-03-29 14:47:38 +0200

    ready for first test case

---
+++ src/test/java/cydeo/day02/SpartanGetRequests.java
@@ -0,0 +1,10 @@
+package cydeo.day02;
+
+public class SpartanGetRequests {
+    String url="http://54.242.181.245:8000/api/spartans";
+ //    Given Accept type application/json
+//    When user send GET request to api/spartans end point
+//    Then status code must 200
+//    And response Content Type must be application/json
+//    And response body should include spartan result
+}
