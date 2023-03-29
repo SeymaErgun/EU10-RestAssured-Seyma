7ce2a2274c42183bc0e456650299fa563b549aa0
seymaergun <seymaergun89@gmail.com>
2023-03-29 16:46:57 +0200

    second test case is done

--- src/test/java/cydeo/day02/SpartanGetRequests.java
+++ src/test/java/cydeo/day02/SpartanGetRequests.java
@@ -5,16 +5,17 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class SpartanGetRequests {
-    String baseurl="http://54.242.181.245:8000";
+
  //    Given Accept type application/json
 //    When user send GET request to api/spartans end point
 //    Then status code must 200
 //    And response Content Type must be application/json
 //    And response body should include spartan result
-
+ String baseurl="http://54.242.181.245:8000";
 
     @Test
     public void test1(){
@@ -38,7 +39,34 @@ public class SpartanGetRequests {
 
     }
 
+ /*
+        Given accept header is application/json
+        When users sends a get request to /api/spartans/3
+        Then status code should be 200
+        And content type should be application/json
+        and json body should contain Fidole
+     */
 
+    @DisplayName("GET one spartan /api/spartans/3 and verify")
+    @Test
+    public void test2(){
+        Response response=RestAssured.given().accept(ContentType.JSON).
+                when().get(baseurl+ "/api/spartans/3" );
+
+        //verify status code is 200
+
+        Assertions.assertEquals(response.statusCode(), 200);
+
+        //verify content type is application/json
+        Assertions.assertEquals(response.contentType(), "application/json");
+
+        //verify json body contains Fidole
+        //(it is not a good usage, because even Fidole is in gender, it will pass because contains in whole string.)
+
+        Assertions.assertTrue(response.body().asString().contains("Fidole"));
+
+
+    }
 
 
 
