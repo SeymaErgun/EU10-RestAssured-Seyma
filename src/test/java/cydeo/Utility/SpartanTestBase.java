b195c633fc48e1a821389250e85c692eb6fee664
seymaergun <seymaergun89@gmail.com>
2023-03-31 13:45:17 +0200

    day3

---
+++ src/test/java/cydeo/Utility/SpartanTestBase.java
@@ -0,0 +1,14 @@
+package cydeo.Utility;
+
+import org.junit.jupiter.api.BeforeAll;
+
+import static io.restassured.RestAssured.baseURI;
+
+public class SpartanTestBase {
+    @BeforeAll
+    public static void init() {
+        //save baseurl inside this variable so that we dont need to type each http method.
+        baseURI = "http://54.242.181.245:8000";  ////change your ip and add :1000 (ip+:1000)
+
+    }
+}
