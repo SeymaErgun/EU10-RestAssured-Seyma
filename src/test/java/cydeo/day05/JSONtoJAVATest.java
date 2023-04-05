63e79c06d538533612b45f2689152e1426ec705f
seymaergun <seymaergun89@gmail.com>
2023-04-05 13:47:04 +0200

    day5 maplist

--- src/test/java/cydeo/day05/JSONtoJAVATest.java
+++ src/test/java/cydeo/day05/JSONtoJAVATest.java
@@ -5,6 +5,7 @@ import io.restassured.response.Response;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
 import java.util.Map;
 
 import static io.restassured.RestAssured.*;
@@ -29,6 +30,21 @@ public class JSONtoJAVATest extends SpartanTestBase {
         String actualName = (String) jsonMap.get("name"); //casting is done
         assertThat(actualName,is("Meta"));
 
+    }
+
+    @DisplayName("GET all spartans to JAVA data structure")
+    @Test
+    public void getAllSpartan(){
+    Response response = get("/api/spartans").then().statusCode(200).extract().response();
+
+     //we need to convert json to java  which is deserialize
+     List<Map<String, Object>> jsonList=response.as(List.class);
+
+        System.out.println(jsonList.get(1).get("name"));
+
+        Map<String, Object> spartan3= jsonList.get(2);
+        System.out.println("spartan3 = " + spartan3);
 
     }
+
 }
