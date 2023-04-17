59f1663d20aa4f2f04c3adc30077b3e7cd5ff583
seymaergun <seymaergun89@gmail.com>
2023-04-17 23:07:41 +0200

    day10

---
+++ src/test/java/cydeo/day10/ResponseTimeTest.java
@@ -0,0 +1,31 @@
+package cydeo.day10;
+
+
+
+import cydeo.Utility.SpartanAuthTestBase;
+import io.restassured.http.*;
+import io.restassured.response.*;
+import org.junit.jupiter.api.*;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+public class ResponseTimeTest extends SpartanAuthTestBase {
+
+
+    @Test
+    public void test1() {
+
+        Response response = given()
+                .auth().basic("admin", "admin")
+                .accept(ContentType.JSON)
+                .when()
+                .get("/api/spartans")
+                .then()
+                .time(both(greaterThan(500L)).and(lessThanOrEqualTo(1500L)))
+                .extract().response();
+
+        System.out.println("response.getTime() = " + response.getTime());
+
+
+    }
+}
